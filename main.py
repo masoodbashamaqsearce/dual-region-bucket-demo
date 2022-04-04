ca2d5cf5e89a2624f94d24fc3ff92ca7d909596c
masoodbashamaqsearce <102943194+masoodbashamaqsearce@users.noreply.github.com>
2022-04-04 11:54:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def replicator():
     proc = subprocess.Popen(["gsutil", "-m", "rsync", "-d", "-r", "gs://masood-mumbai", "gs://masood-delhi"])
-    print("gsutil called")
     try:
         outs, errs = proc.communicate(timeout=60)
     except subprocess.TimeoutExpired:
         proc.kill()
         outs, errs = proc.communicate()
     print(outs)
-    print(errs)
-    return "1"
+    if outs is not None:
+        return outs
+    else:
+        return "1"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
