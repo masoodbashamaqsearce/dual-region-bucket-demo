15d740dbc3c5b5f01badaf203c5a77a6b1a111a7
masoodbashamaqsearce <102943194+masoodbashamaqsearce@users.noreply.github.com>
2022-04-04 18:10:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def replicator(event=None):
     print("events:",event)
     proc = subprocess.Popen(["gsutil", "-m", "rsync", "-d", "-r", "gs://masood-mumbai", "gs://masood-delhi"])
