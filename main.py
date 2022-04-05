a28ea4b9093aebc75efed33fc7c559667e35102b
masoodbashamaqsearce <102943194+masoodbashamaqsearce@users.noreply.github.com>
2022-04-05 18:05:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=['POST'])
 def replicator(__GCP_CloudEventsMode=None):
     print(__GCP_CloudEventsMode)
+    subprocess.Popen(["gsutil", "-m", "rsync", "gs://masood-mumbai/100gb_file", "gs://masood-delhi/"])
     return "1"
 
 if __name__ == "__main__":
