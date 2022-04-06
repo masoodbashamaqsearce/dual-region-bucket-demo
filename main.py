ba0c665ed90f2f167b107894b2eb9c56fb04ba69
masoodbashamaqsearce <102943194+masoodbashamaqsearce@users.noreply.github.com>
2022-04-06 12:30:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import os
-from flask import Flask
+from flask import Flask, request
 import subprocess
+import base64
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
-def replicator(__GCP_CloudEventsMode=None):
-    print(__GCP_CloudEventsMode)
-    subprocess.Popen(["gsutil", "-m", "rsync", "gs://masood-mumbai/100gb_file", "gs://masood-delhi/"])
-    return "1"
+def replicator():
+    data = request.get_json()
+    if not data:
+        msg = 'no Pub/Sub message received'
+        print(f'error: {msg}')
+        return f'Bad Request: {msg}', 400
+
+    if not isinstance(data, dict) or 'message' not in data:
+        msg = 'invalid Pub/Sub message format'
+        print(f'error: {msg}')
+        return f'Bad Request: {msg}', 400
+
+    pubsub_message = data['message']
+    name = 'World'
+    if isinstance(pubsub_message, dict) and 'data' in pubsub_message:
+        name = base64.b64decode(pubsub_message['data']).decode('utf-8').strip()
+        
+    resp = f"Hello, {name}! ID: {request.headers.get('ce-id')}"
+    print(resp)
+    return (resp, 200)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
