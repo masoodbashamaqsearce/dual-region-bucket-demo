e5b4c817d7f38b9ad740cead5d70fd56a0f4d2d4
masood bashamaq <masood.bashamaq@searce.com>
2022-04-18 12:53:02 +0530

    d

--- main.py
+++ main.py
@@ -91,15 +91,18 @@ def delete():
     logging_client = logging.Client()
     logging_client.setup_logging()
     data = request.get_json()
-    pubsub_message = data['message']
-    log.info("delete method called...")
-    name = 'World'
-    if isinstance(pubsub_message, dict) and 'data' in pubsub_message:
-        name = base64.b64decode(pubsub_message['data']).decode('utf-8').strip()
-        
-    resp = f"Hello, {name}! ID: {request.headers.get('ce-id')}"
-    log.info(name)
-    return (resp, 200)
+    log.info("root method called...")
+    wholedata = f"wholedata : {data} ::end of data"
+    log.info(wholedata)
+    prt = "data['protoPayload']['methodName']:"+data['protoPayload']['methodName']
+    log.info(prt)
+    prt = "data['protoPayload']['resourceName']:"+data['protoPayload']['resourceName']
+    log.info(prt)
+    prt = "data['resource']['labels']['bucket_name']:"+data['resource']['labels']['bucket_name']
+    log.info(prt)
+    prt = "data['resource']['labels']['location']:"+data['resource']['labels']['location']
+    log.info(prt)
+    return ('OK', 200)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
