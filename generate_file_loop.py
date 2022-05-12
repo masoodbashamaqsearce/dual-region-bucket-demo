3e68fb7ecbbccae66ce129796e67980f5a47ed04
masoodbashamaqsearce <102943194+masoodbashamaqsearce@users.noreply.github.com>
2022-05-12 14:11:55 +0530

    Update generate_file_loop.py

--- generate_file_loop.py
+++ generate_file_loop.py
@@ -1,11 +1,12 @@
 import os
 import subprocess
 dest = "gs://masood-mumbai-2/auto/"
+max_size=2147483648
 for x in range (1,1000):
     print(str(x)+".txt started..")
     fn = str(x) + ".txt"
     f = open(fn,"wb")
-    f.seek(120000-1)
+    f.seek(random.randint(10, max_size)-1)
     f.write(b"\0")
     f.close()
     os.stat(fn).st_size
